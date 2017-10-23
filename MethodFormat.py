97ddfc0e3bf40a94db082380880f43e42f1b4030
spaceindependence <spaceindependence@protonmail.com>
2017-10-23 15:46:38 +0300

    MethodPython.py was updated

--- MethodFormat.py
+++ MethodFormat.py
@@ -2,3 +2,4 @@ age = 22
 name = 'Alexandra'
 print('Age of {} -- {} years.'.format(name, age))
 print('Why {} having fun with this Python?'.format(name))
+print('{name} loves {boyfriend} a lot.' .format(name='Sanechka', boyfriend='Igoreshka'))
